f0c398d26d8c7b597f43359b087b629db513dd8f
AbdullahAsif24 <abdullahasif4045@gmail.com>
2025-08-06 21:29:12 -0700

    .

--- leetcode.java
+++ leetcode.java
@@ -298,7 +298,51 @@ public class leetcode {
         return i - 1; // floor of sqrt
     }
 
-    
+    /*
+     * ===================Merge Sorted Array============
+     * You are given two integer arrays nums1 and nums2, sorted in non-decreasing
+     * order, and two integers m and n, representing the number of elements in nums1
+     * and nums2 respectively.
+     * 
+     * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+     * 
+     * The final sorted array should not be returned by the function, but instead be
+     * stored inside the array nums1. To accommodate this, nums1 has a length of m +
+     * n, where the first m elements denote the elements that should be merged, and
+     * the last n elements are set to 0 and should be ignored. nums2 has a length of
+     * n.
+     * 
+     * 
+     * 
+     * Example 1:
+     * 
+     * Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+     * Output: [1,2,2,3,5,6]
+     * Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+     * The result of the merge is [1,2,2,3,5,6] with the underlined elements coming
+     * from nums1.
+     */
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int total = m; // track how many elements are valid in nums1
+
+        for (int i = 0; i < n; i++) {
+            int insertIndex = 0;
+            // Find the correct position to insert nums2[i]
+            while (insertIndex < total && nums1[insertIndex] <= nums2[i]) {
+                insertIndex++;
+            }
+
+            // Shift elements to the right
+            for (int j = m + i; j > insertIndex; j--) {
+                nums1[j] = nums1[j - 1];
+            }
+
+            // Insert the element
+            nums1[insertIndex] = nums2[i];
+            total++;
+        }
+    }
 
     public static void main(String[] args) {
         String haystack = "leetcode", needle = "leeto";
