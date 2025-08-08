38f0dc795a11498496675dde8e9889a2bcd7b182
AbdullahAsif24 <abdullahasif4045@gmail.com>
2025-08-07 21:22:22 -0700

    .

--- leetcode.java
+++ leetcode.java
@@ -344,6 +344,28 @@ public class leetcode {
         }
     }
 
+    /*
+     * ================ Length of Last Word================
+     * Given a string s consisting of words and spaces, return the length of the
+     * last word in the string.
+     * 
+     * A word is a maximal substring consisting of non-space characters only.
+     * 
+     * 
+     * 
+     * Example 1:
+     * 
+     * Input: s = "Hello World"
+     * Output: 5
+     * Explanation: The last word is "World" with length 5.
+     */
+
+    public static int lengthOfLastWord(String s) {
+        String[] str = s.split(" ");
+        String fin = str[str.length - 1];
+        return fin.length();
+    }
+
     public static void main(String[] args) {
         String haystack = "leetcode", needle = "leeto";
 
