08b10280e3ac89ce0d4dd7ea9be67f8550706397
AbdullahAsif24 <abdullahasif4045@gmail.com>
2025-08-10 08:14:51 -0700

    .

--- leetcode.java
+++ leetcode.java
@@ -367,9 +367,40 @@ public class leetcode {
         return fin.length();
     }
 
-    public static void main(String[] args) {
-        String haystack = "leetcode", needle = "leeto";
+    /*
+     * ====================== Add Binary====================
+     * Given two binary strings a and b, return their sum as a binary string.
+     * 
+     * 
+     * 
+     * Example 1:
+     * 
+     * Input: a = "11", b = "1"
+     * Output: "100"
+     */
 
-        System.out.println(strStr(haystack, needle));
+    public static String addBinary(String a, String b) {
+
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+            if (i >= 0)
+                sum += a.charAt(i--) - '0';
+            if (j >= 0)
+                sum += b.charAt(j--) - '0';
+            result.append(sum % 2);
+            carry = sum / 2;
+        }
+
+        return result.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(addBinary("11", "1"));
     }
 }
