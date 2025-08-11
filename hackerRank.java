fd16d2bd68895d44b2c2f55e01032880ed09b67d
AbdullahAsif24 <abdullahasif4045@gmail.com>
2025-08-10 21:23:23 -0700

    .

--- hackerRank.java
+++ hackerRank.java
@@ -1,34 +1,64 @@
+import java.util.Scanner;
+
 public class hackerRank {
 
     public static String getSmallestAndLargest(String s, int k) {
         String smallest = "";
         String largest = "";
+
         
-        // Complete the function
         // 'smallest' must be the lexicographically smallest substring of length 'k'
         // 'largest' must be the lexicographically largest substring of length 'k'
-        
-        String[] subS = new String[s.length()-k+1];
-        
-        for(int i = 0; i<s.length()-k+1; i++){
-            subS[i] = s.substring(i, i+k);
+
+        String[] subS = new String[s.length() - k + 1];
+
+        for (int i = 0; i < s.length() - k + 1; i++) {
+            subS[i] = s.substring(i, i + k);
         }
 
         smallest = subS[0];
         largest = subS[0];
         for (String string : subS) {
-            if (string.compareTo(smallest) < 0 ) {
+            if (string.compareTo(smallest) < 0) {
                 smallest = string;
             }
             if (string.compareTo(largest) > 0) {
                 largest = string;
             }
         }
-        
+
         return smallest + "\n" + largest;
     }
 
+    public static void isPalindromeFunc() {
+
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+
+        boolean isPalindrome = true;
+        int mid = A.length() / 2;
+
+        int start = 0;
+        int end = A.length() - 1;
+        while (start < mid && end > mid) {
+            if (A.charAt(start) != A.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(getSmallestAndLargest("welcometojava", 3));
+        isPalindromeFunc();
     }
 }
