eaffd34ed7a5091aa5fb7c4775550656a908d646
AbdullahAsif24 <abdullahasif4045@gmail.com>
2025-08-10 20:35:02 -0700

    .

--- leetcode.java
+++ leetcode.java
@@ -399,8 +399,44 @@ public class leetcode {
         return result.reverse().toString();
     }
 
+    /*
+     * ======================= Remove Element ================
+     * Given an integer array nums and an integer val, remove all occurrences of val
+     * in nums in-place. The order of the elements may be changed. Then return the
+     * number of elements in nums which are not equal to val.
+     * 
+     * Consider the number of elements in nums which are not equal to val be k, to
+     * get accepted, you need to do the following things:
+     * 
+     * Change the array nums such that the first k elements of nums contain the
+     * elements which are not equal to val. The remaining elements of nums are not
+     * important as well as the size of nums.
+     * Return k.
+     * 
+     * Example 2:
+     * 
+     * Input: nums = [0,1,2,2,3,0,4,2], val = 2
+     * Output: 5, nums = [0,1,4,0,3,_,_,_]
+     */
+
+    public static int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int num : nums) {
+            if (num != val) {
+                nums[k++] = num;
+            }
+        }
+        return k;
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(addBinary("11", "1"));
+        int[] arr = { 0, 1, 2, 2, 3, 0, 4, 2 };
+        System.out.println(removeElement(arr, 2));
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
+
     }
 }
