9a27a2c71f942a908e26a2f86fded5738a54db88
AbdullahAsif24 <abdullahasif4045@gmail.com>
2025-08-18 21:00:38 -0700

    .

--- leetcode.java
+++ leetcode.java
@@ -7,16 +7,16 @@ public class leetcode {
      * ================= Q1 ======================
      * Given an array of integers nums and an integer target, return indices of the
      * two numbers such that they add up to target.
-     * 
+     *
      * You may assume that each input would have exactly one solution, and you may
      * not use the same element twice.
-     * 
+     *
      * You can return the answer in any order.
-     * 
-     * 
-     * 
+     *
+     *
+     *
      * Example 1:
-     * 
+     *
      * Input: nums = [2,7,11,15], target = 9
      * Output: [0,1]
      * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
@@ -26,18 +26,18 @@ public class leetcode {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
+                    return new int[]{i, j};
                 }
             }
         }
-        return new int[] {};
+        return new int[]{};
     }
 
     /*
      * ================= Q2 ======================
      * Roman numerals are represented by seven different symbols: I, V, X, L, C, D
      * and M.
-     * 
+     *
      * Symbol Value
      * I 1
      * V 5
@@ -49,19 +49,19 @@ public class leetcode {
      * For example, 2 is written as II in Roman numeral, just two ones added
      * together. 12 is written as XII, which is simply X + II. The number 27 is
      * written as XXVII, which is XX + V + II.
-     * 
+     *
      * Roman numerals are usually written largest to smallest from left to right.
      * However, the numeral for four is not IIII. Instead, the number four is
      * written as IV. Because the one is before the five we subtract it making four.
      * The same principle applies to the number nine, which is written as IX. There
      * are six instances where subtraction is used:
-     * 
+     *
      * I can be placed before V (5) and X (10) to make 4 and 9.
      * X can be placed before L (50) and C (100) to make 40 and 90.
      * C can be placed before D (500) and M (1000) to make 400 and 900.
      * Given a roman numeral, convert it to an integer.
-     * 
-     * 
+     *
+     *
      */
     public static int romanToInt(String s) {
         HashMap<Character, Integer> romanMap = new HashMap<>();
@@ -98,18 +98,18 @@ public class leetcode {
      * ================= Q3 ======================
      * Given a string s containing just the characters '(', ')', '{', '}', '[' and
      * ']', determine if the input string is valid.
-     * 
+     *
      * An input string is valid if:
-     * 
+     *
      * Open brackets must be closed by the same type of brackets.
      * Open brackets must be closed in the correct order.
      * Every close bracket has a corresponding open bracket of the same type.
-     * 
-     * 
+     *
+     *
      * Example 1:
-     * 
+     *
      * Input: s = "()"
-     * 
+     *
      * Output: true
      */
     public static boolean isValid(String s) {
@@ -138,13 +138,13 @@ public class leetcode {
      * each digits[i] is the ith digit of the integer. The digits are ordered from
      * most significant to least significant in left-to-right order. The large
      * integer does not contain any leading 0's.
-     * 
+     *
      * Increment the large integer by one and return the resulting array of digits.
-     * 
-     * 
-     * 
+     *
+     *
+     *
      * Example 1:
-     * 
+     *
      * Input: digits = [1,2,3]
      * Output: [1,2,4]
      * Explanation: The array represents the integer 123.
@@ -177,30 +177,30 @@ public class leetcode {
      * ================= Remove Duplicates from sorted Array ======================
      * Consider the number of unique elements of nums to be k, to get accepted, you
      * need to do the following things:
-     * 
+     *
      * Change the array nums such that the first k elements of nums contain the
      * unique elements in the order they were present in nums initially. The
      * remaining elements of nums are not important as well as the size of nums.
      * Return k.
      * Custom Judge:
-     * 
+     *
      * The judge will test your solution with the following code:
-     * 
+     *
      * int[] nums = [...]; // Input array
      * int[] expectedNums = [...]; // The expected answer with correct length
-     * 
+     *
      * int k = removeDuplicates(nums); // Calls your implementation
-     * 
+     *
      * assert k == expectedNums.length;
      * for (int i = 0; i < k; i++) {
      * assert nums[i] == expectedNums[i];
      * }
      * If all assertions pass, then your solution will be accepted.
-     * 
-     * 
-     * 
+     *
+     *
+     *
      * Example 1:
-     * 
+     *
      * Input: nums = [1,1,2]
      * Output: 2, nums = [1,2,_]
      * Explanation: Your function should return k = 2, with the first two elements
@@ -226,7 +226,7 @@ public class leetcode {
      * Given a sorted array of distinct integers and a target value, return the
      * index if the target is found. If not, return the index where it would be if
      * it were inserted in order.
-     * 
+     *
      * Input: nums = [1,3,5,6], target = 2
      * Output: 1
      */
@@ -247,11 +247,11 @@ public class leetcode {
      * String===============
      * Given two strings needle and haystack, return the index of the first
      * occurrence of needle in haystack, or -1 if needle is not part of haystack.
-     * 
-     * 
-     * 
+     *
+     *
+     *
      * Example 1:
-     * 
+     *
      * Input: haystack = "sadbutsad", needle = "sad"
      * Output: 0
      * Explanation: "sad" occurs at index 0 and 6.
@@ -271,14 +271,14 @@ public class leetcode {
      * ===========================Sqrt(x)======================
      * Given a non-negative integer x, return the square root of x rounded down to
      * the nearest integer. The returned integer should be non-negative as well.
-     * 
+     *
      * You must not use any built-in exponent function or operator.
-     * 
+     *
      * For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
-     * 
-     * 
+     *
+     *
      * Example 1:
-     * 
+     *
      * Input: x = 4
      * Output: 2
      * Explanation: The square root of 4 is 2, so we return 2.
@@ -303,19 +303,19 @@ public class leetcode {
      * You are given two integer arrays nums1 and nums2, sorted in non-decreasing
      * order, and two integers m and n, representing the number of elements in nums1
      * and nums2 respectively.
-     * 
+     *
      * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
-     * 
+     *
      * The final sorted array should not be returned by the function, but instead be
      * stored inside the array nums1. To accommodate this, nums1 has a length of m +
      * n, where the first m elements denote the elements that should be merged, and
      * the last n elements are set to 0 and should be ignored. nums2 has a length of
      * n.
-     * 
-     * 
-     * 
+     *
+     *
+     *
      * Example 1:
-     * 
+     *
      * Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
      * Output: [1,2,2,3,5,6]
      * Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
@@ -348,13 +348,13 @@ public class leetcode {
      * ================ Length of Last Word================
      * Given a string s consisting of words and spaces, return the length of the
      * last word in the string.
-     * 
+     *
      * A word is a maximal substring consisting of non-space characters only.
-     * 
-     * 
-     * 
+     *
+     *
+     *
      * Example 1:
-     * 
+     *
      * Input: s = "Hello World"
      * Output: 5
      * Explanation: The last word is "World" with length 5.
@@ -370,11 +370,11 @@ public class leetcode {
     /*
      * ====================== Add Binary====================
      * Given two binary strings a and b, return their sum as a binary string.
-     * 
-     * 
-     * 
+     *
+     *
+     *
      * Example 1:
-     * 
+     *
      * Input: a = "11", b = "1"
      * Output: "100"
      */
@@ -404,17 +404,17 @@ public class leetcode {
      * Given an integer array nums and an integer val, remove all occurrences of val
      * in nums in-place. The order of the elements may be changed. Then return the
      * number of elements in nums which are not equal to val.
-     * 
+     *
      * Consider the number of elements in nums which are not equal to val be k, to
      * get accepted, you need to do the following things:
-     * 
+     *
      * Change the array nums such that the first k elements of nums contain the
      * elements which are not equal to val. The remaining elements of nums are not
      * important as well as the size of nums.
      * Return k.
-     * 
+     *
      * Example 2:
-     * 
+     *
      * Input: nums = [0,1,2,2,3,0,4,2], val = 2
      * Output: 5, nums = [0,1,4,0,3,_,_,_]
      */
@@ -429,14 +429,31 @@ public class leetcode {
         return k;
     }
 
+    /*
+    *
+
+    ============================ Power of Three ======================
+
+    Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+    An integer n is a power of three, if there exists an integer x such that n == 3x.
+    *
+    * Example 1:
+
+Input: n = 27
+Output: true
+Explanation: 27 = 33
+    * */
+
+    public static boolean isPowerOfThree(int n) {
+
+        int maxPowerOf3 = 1162261467; // 3^19 is the largest power of 3 in int range
+        return n > 0 && maxPowerOf3 % n == 0;
+    }
+
     public static void main(String[] args) {
 
-        int[] arr = { 0, 1, 2, 2, 3, 0, 4, 2 };
-        System.out.println(removeElement(arr, 2));
-
-        for (int i : arr) {
-            System.out.println(i);
-        }
+        System.out.println(isPowerOfThree(5));
 
     }
 }
